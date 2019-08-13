6bbe6d2a9eadf7d6fa63ac265f166b251ee49c9f
Andrew Pitt <apitt@redhat.com>
2019-08-13 19:20:55 -0400

    Added to selenium test.

--- src/test/java/integration/GoogleIT.java
+++ src/test/java/integration/GoogleIT.java
@@ -1,11 +1,14 @@
 package integration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.net.MalformedURLException;
 
 import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class GoogleIT {
 
@@ -14,7 +17,6 @@ public class GoogleIT {
 
 	public GoogleIT() throws MalformedURLException {
 		config = new SeleniumConfig();
-		config.getDriver().get(url);
 	}
 
 	public String getTitle() {
@@ -28,8 +30,17 @@ public class GoogleIT {
 
 	@Test
 	public void googleTitleIT() {
-		config.getDriver().get(url);
+		WebDriver driver = config.getDriver();
+		driver.get(url);
 		assertEquals("Title not as expected: ", "Incredible Random Number Generator", getTitle());
+		// Save the random value from the page.
+		String value1 = driver.findElement(By.id("random-value")).getText();
+		// Reload the page and get a new random value.
+		driver.get(url);
+		String value2 = driver.findElement(By.id("random-value")).getText();
+		// Values should not be the same.
+		assertFalse("Values should not be the same.", value1.contentEquals(value2));
+		System.out.println("Values: " + value1 + ", " + value2);
 	}
 
 }
